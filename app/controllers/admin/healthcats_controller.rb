4dab186954de3d51819af266e044ccf95a672b38
pai-pai <paimai.dororo@gmail.com>
2012-10-05 04:02:47 -0700

    Edit layers

---
+++ app/controllers/admin/healthcats_controller.rb
@@ -0,0 +1,54 @@
+class Admin::HealthcatsController < ApplicationController
+    load_and_authorize_resource
+
+    def index
+        @healthcats = Healthcat.may_be_a_parent
+    end
+
+    def new
+        @healthcat = Healthcat.new
+    end
+
+    def create
+        @healthcat = Healthcat.new(params[:healthcat])
+        if params[:cancel_button] || @healthcat.save
+            redirect_to healthcats_path
+        else
+            render :new
+        end
+    end
+
+    def edit
+        @healthcat = Healthcat.find(params[:id])
+    end
+
+    def update
+        @healthcat = Healthcat.find(params[:id])
+        if params[:cancel_button] || @healthcat.update_attributes(params[:healthcat])
+            redirect_to healthcats_path
+        else
+            render :edit
+        end
+    end
+
+    def show
+        @healthcat = Healthcat.find(params[:id])
+        @child_healthcats = @healthcat.child_healthcats
+    end
+
+    def show_child
+        @healthcat = Healthcat.find(params[:id])
+    end
+
+    def destroy
+        @healthcat = Healthcat.find(params[:id])
+        @child_healthcats = @healthcat.child_healthcats
+        if not @child_healthcats.blank?
+            @child_healthcats.each do |child|
+                Healthcat.find(child.id).destroy
+            end
+        end
+        @healthcat.destroy
+        redirect_to healthcats_path
+    end
+end
