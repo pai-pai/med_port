804fefb79ebcdbfd6b0e937ae643ca032a1bd989
pai-pai <paimai.dororo@gmail.com>
2012-11-25 23:24:37 +0400

    Parallax

--- app/assets/javascripts/parallax.js
+++ app/assets/javascripts/parallax.js
@@ -1,24 +1,28 @@
 var reObj;
 var xPos;
 var coords;
+var speed;
 
-function redraw(obj, shift, y){
-    $reObj = obj;
-    xPos = -($(window).scrollLeft() * $reObj.data('speed')) - shift;
+function redraw(obj, shift, y, moveto){
+    xPos = $(window).scrollLeft() * obj.data('speed');
+    if (moveto == 'left') { xPos = -(xPos) };
+    xPos = xPos - shift;
     coords = xPos + 'px ' + y;
-    $reObj.css({ backgroundPosition: coords });
+    obj.css({ backgroundPosition: coords });
 }
 
 $(document).ready(function(){
     $(window).scroll(function() {
-        redraw( $('#day_sky'), 0, '50%' );
-        redraw( $('#day_forest'), 0, '150px' );
-        redraw( $('#day_trees'), 1000, '159px' );
-        redraw( $('#day_ducks'), (-1366), '582px' );
-        if ($(window).scrollLeft() > 100 && $(window).scrollLeft() < 350) {
-            $('#day_info_box').show();
+        redraw( $('#day_sky'), 0, '50%', 'right' );
+        redraw( $('#night_sky'), 0, '50%', 'left' );
+        redraw( $('#evening_forest'), 0, '150px', 'left' );
+        redraw( $('#day_forest'), 0, '150px', 'left' );
+        redraw( $('#day_trees'), 1000, '159px', 'left' );
+        redraw( $('#day_ducks'), 800, '582px', 'right' );
+        if ($(window).scrollLeft() > 125 && $(window).scrollLeft() < 675) {
+            $('#day_info_box').fadeIn();
         } else {
-            $('#day_info_box').hide();
+            $('#day_info_box').fadeOut();
         };
     });
 });
